c0b0b2ae04d4de80fae7c3e14abc1d299fae149a
Inshal Ansari <inshalansari80@gmail.com>
2021-11-21 14:29:41 +0530

    Initial Commit

---
+++ components/TabIcon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TabIcon = () => {
+    return (
+        <View>
+            <Text>Tab Icon Woriking</Text>
+        </View>
+    )
+}
+
+export default TabIcon
+
+const styles = StyleSheet.create({})
